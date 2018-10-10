bf84b0ef750a29d4940d10eb1ca6559ed582fc9a
Donald Labaj <donald.labaj@gmail.com>
2018-10-10 00:22:59 -0400

    Updated status and cards.

--- src/components/tutorialDashboard/tutorialDashboard.js
+++ src/components/tutorialDashboard/tutorialDashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import PropTypes from 'prop-types';
 import { CardGrid, Col, Row, Icon } from 'patternfly-react';
 import TutorialCard from '../tutorialCard/tutorialCard';
